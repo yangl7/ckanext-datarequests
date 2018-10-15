ffead9e6d7c566628f57bbd4019ffbf9c48a5321
yangl7 <yangl7@nationwide.com>
2018-10-14 20:06:45 -0400

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -6,16 +6,19 @@ echo "This is travis-build.bash..."
 echo "Installing the packages that CKAN requires..."
 # sudo apt-get update -qq
 # sudo apt-get install solr-jetty
-sudo yum update -y
-sudo yum install solr-jetty
+#sudo yum update -y
+#sudo yum install solr-jetty
 
 echo "Installing CKAN and its Python dependencies..."
-git clone https://github.com/ckan/ckan
-cd ckan
-git checkout ckan-$CKANVERSION
-python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+#git clone https://github.com/ckan/ckan
+cd ~/Projects/ckan/2.8.1/src
+#git checkout ckan-$CKANVERSION
+echo "-----------------------------------------------------------------------"
+~/.pyenv/shims/pip install -r requirement-setuptools.txt
+#python setup.py develop
+~/.pyenv/shims/pip install -r requirements.txt #--allow-all-external
+~/.pyenv/shims/pip install -r dev-requirements.txt #--allow-all-external
+~/.pyenv/shims/pip install .
 cd -
 
 echo "Setting up Solr..."
@@ -28,18 +31,17 @@ echo "Setting up Solr..."
 #sudo service jetty restart
 
 echo "Creating the PostgreSQL user and database..."
-sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
-sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
-sudo -u postgres psql -c "CREATE DATABASE ckan_test WITH OWNER ckan_default;"
-sudo -u postgres psql -c "CREATE DATABASE datastore_test WITH OWNER ckan_default;"
-
+#docker exec -it ckan_postgres psql -U ckan -c "DROP ROLE IF EXISTS ckan_default; CREATE USER ckan_default WITH PASSWORD 'pass';"
+#docker exec -it ckan_postgres psql -U ckan -c "DROP ROLE IF EXISTS datastore_default; CREATE USER datastore_default WITH PASSWORD 'pass';"
+#docker exec -it ckan_postgres psql -U ckan -c "CREATE DATABASE ckan_test WITH OWNER ckan_default;"
+#docker exec -it ckan_postgres psql -U ckan -c "CREATE DATABASE datastore_test WITH OWNER ckan_default;"
 
 echo "Initialising the database..."
-cd ckan
-paster db init -c test-core.ini
+#cd ckan
+#paster db init -c /var/lib/jenkins/Projects/ckan/2.8.1/src/test-core.ini
 cd -
 
 echo "Installing ckanext-datarequests and its requirements..."
-python setup.py develop
+#sudo python setup.py develop
 
 echo "travis-build.bash is done."
